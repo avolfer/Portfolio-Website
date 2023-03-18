3fa3cfa1a2615f21a4657a2d03efccf7c34519a7
avolfer <jakubstuba2004@gmail.com>
2023-03-18 16:22:42 +0100

    modified:   changeBackground.js new file:   img/broken-link.png new file:   img/day-and-night.png new file:   img/github.png modified:   index.html modified:   style.css

---
+++ changeBackground.js
@@ -0,0 +1,14 @@
+const body = document.body;
+const button = document.getElementById("changeColorBtn");
+const originalBackgroundColor = body.style.backgroundColor;
+let isColorChanged = false;
+
+button.onclick = function () {
+  if (isColorChanged) {
+    body.style.backgroundColor = originalBackgroundColor;
+    isColorChanged = false;
+  } else {
+    body.style.backgroundColor = "#999999";
+    isColorChanged = true;
+  }
+};
